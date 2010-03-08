f06b28c433ab3a6528801ff529fc6f52638556ca
Donald Piret <donald@donaldpiret.com>
2010-03-08 15:13:00 +0100

    Update to the way things work

--- lib/mailchimp_fu/acts/mailchimp_subscriber.rb
+++ lib/mailchimp_fu/acts/mailchimp_subscriber.rb
@@ -1,5 +1,6 @@
 require 'xmlrpc/client'
 require 'mail_chimp'
+
 module DonaldPiret
   module MailchimpFu
     
@@ -21,12 +22,13 @@ module DonaldPiret
         mattr_reader :mailchimp_config
         mattr_reader :mailchimp_apikey
         begin
-          @@mailchimp_config = YAML.load(File.open("#{ENV['RAILS_ROOT']}/config/mailchimp_fu.yml"))[ENV["RAILS_ENV"]].symbolize_keys
-          @@mailchimp_apikey = MailChimp.login(@@mailchimp_config[:username], @@mailchimp_config[:password]) unless ENV["RAILS_ENV"] == 'test'
+          @@mailchimp_config = YAML.load(File.open("#{RAILS_ROOT}/config/mailchimp_fu.yml"))[Rails.env.to_s].symbolize_keys rescue nil
+          @@mailchimp_apikey = MailChimp.login(@@mailchimp_config[:username], @@mailchimp_config[:password])
+        rescue
         end
         base.instance_eval do
           after_create :after_mailchimp_subscriber_create
-          before_update :before_mailchimp_subscriber_update
+          after_update :before_mailchimp_subscriber_update
           after_destroy :after_mailchimp_subscriber_destroy
         end
       end
@@ -41,48 +43,120 @@ module DonaldPiret
         end
       end
       
+      def mailchimp_merge_var_changed?(key)
+        value = self.mailchimp_merge_vars[key]
+        if value.is_a? Symbol
+          self.respond_to?("#{value}_changed?") && self.send("#{value}_changed?")
+        else
+          return false
+        end
+      end
+      
+      def merge_vars_changed?
+        changed = false
+        self.mailchimp_merge_vars.each { |mv|
+          changed ||= mailchimp_merge_var_changed?(mv) unless mailchimp_merge_var(mv).nil?
+        }
+      end
+      
       # After subscriber created callback
       # Do initial list registration
       def after_mailchimp_subscriber_create
-        email_address = self.send(self.mailchimp_email_column.to_sym)
-        merge_vars = {}
-        self.mailchimp_merge_vars.each { |mv|
-          merge_vars[mv.to_sym] = mailchimp_merge_var(mv)
-        }
-        MailChimp.list_subscribe(self.mailchimp_list_name, email_address, merge_vars)
+        wants_email = self.send(self.mailchimp_enabled_column.to_sym)
+        if wants_email
+          logger.info "Calling MailChimpSubscriber subscribe after create"
+          self.subscribe_to_mailchimp
+        end
       end
-      handle_asynchronously :after_mailchimp_subscriber_create if defined?(Delayed::MessageSending) && !Rails.env.test?
-      
-      
-      # before subscriber update callback
+            
+      # Before subscriber update callback
       # Do list update
-      def after_mailchimp_subscriber_update
-        if self.send("#{self.mailchimp_email_column}_changed?") 
-          email_address = self.send("#{self.mailchimp_email_column}_was")
-          merge_vars = {:EMAIL => self.send(self.mailchimp_email_column.to_sym)}
-        else
-          email_address = self.send(self.mailchimp_email_column.to_sym)
-          merge_vars = {}
+      def before_mailchimp_subscriber_update
+        wants_email = self.send(self.mailchimp_enabled_column.to_sym)
+        wants_email_changed = self.send((self.mailchimp_enabled_column.to_s + "_changed?").to_sym)
+        if wants_email_changed
+          if wants_email
+            logger.info "Calling MailChimpSubscriber subscribe after update"
+            self.subscribe_to_mailchimp
+          else
+            old_email = self.send("#{self.mailchimp_email_column}_was") || self.send("#{self.mailchimp_email_column}")
+            logger.info "Calling MailChimpSubscriber unsubscribe after update"
+            self.unsubscribe_from_mailchimp(old_email)
+          end
+        elsif wants_email
+          email_changed = self.send("#{self.mailchimp_email_column}_changed?")
+          if email_changed || merge_vars_changed?
+            old_email = self.send("#{self.mailchimp_email_column}_was") || self.send("#{self.mailchimp_email_column}")
+            logger.info "Calling MailchimpWorker Async Update after update"
+            self.update_mailchimp_subscription(old_email)
+          end
         end
-        self.mailchimp_merge_vars.each { |mv|
-          merge_vars[mv.to_sym] = mailchimp_merge_var(mv)
-        }
-        # Subscribe or unsubscribe the user if the enabled field has changed
-        if self.mailchimp_enabled_column && self.send("#{self.mailchimp_enabled_column}_changed?")
-          self.send(self.mailchimp_enabled_column.to_sym) ? MailChimp.list_subscribe(self.mailchimp_list_name, email_address) : MailChimp.list_unsubscribe(self.mailchimp_list_name, email_address)
-        end
-        # Then update the member
-        MailChimp.list_update_member(self.mailchimp_list_name, email_address, merge_vars)
       end
-      handle_asynchronously :before_mailchimp_subscriber_update if defined?(Delayed::MessageSending) && !Rails.env.test?
       
       # After subscriber destroy callback
       # Remove from list
       def after_mailchimp_subscriber_destroy
-        email_address = self.send(self.mailchimp_email_column.to_sym)
-        MailChimp.list_unsubscribe(self.mailchimp_list_name, email_address)
+        logger.info "Calling MailchimpWorker Async Unsusbscribe after destroy"
+        self.unsubscribe_from_mailchimp
       end
-      handle_asynchronously :after_mailchimp_subscriber_destroy if defined?(Delayed::MessageSending) && !Rails.env.test?
+      
+      def subscribe_to_mailchimp
+        email_address = self.send(self.mailchimp_email_column.to_sym)
+        merge_vars = {}
+        self.mailchimp_merge_vars.each { |mv|
+          merge_vars[mv.to_sym] = mailchimp_merge_var(mv) unless mailchimp_merge_var(mv).nil?
+        }
+        MailChimp.list_subscribe(self.mailchimp_list_name, email_address, merge_vars)
+        logger.info "Called MailChimp.list_subscribe member on #{email_address}"
+      rescue => e
+        if e.message =~ /is already subscribed to list/
+          # User is already subscribed to list, so update instead
+          logger.info "Calling MailChimpSubscriber update after create"
+          self.update_mailchimp_subscription
+        else
+          raise e  
+        end
+      end
+      
+      def update_mailchimp_subscription(old_email = nil)
+        email_address = old_email || self.send(self.mailchimp_email_column.to_sym)
+        merge_vars = {}
+        self.mailchimp_merge_vars.each { |mv|
+          merge_vars[mv.to_sym] = mailchimp_merge_var(mv) unless mailchimp_merge_var(mv).nil?
+        }
+        merge_vars = merge_vars.merge(:email => self.send(self.mailchimp_email_column.to_sym)) if old_email != self.send(self.mailchimp_email_column.to_sym)
+        MailChimp.list_update_member(self.mailchimp_list_name, email_address, merge_vars)
+        logger.info "Called MailChimp.list_update member on #{email_address}"
+      rescue => e
+        if e.message =~ /is already subscribed to list/
+          # User is already subscribed to list, so update instead
+          self.update_mailchimp_subscription(old_email)
+        elsif e.message.strip =~ /^There is no record of "(.+)" in the database$/
+          self.subscribe_to_mailchimp
+        elsif e.message.strip =~ /^The email address "(.+)" does not belong to this list$/
+          self.update_attribute(self.mailchimp_enabled_column.to_sym, false)
+        else
+          raise e  
+        end
+      end
+      
+      def unsubscribe_from_mailchimp(old_email = nil)
+        email_address = old_email || self.send(self.mailchimp_email_column.to_sym)
+        MailChimp.list_unsubscribe(self.mailchimp_list_name, email_address)
+        logger.info "Called MailChimp.list_unsubscribe member on #{email_address}"
+      rescue => e
+        if e.message.strip =~ /^There is no record of "(.+)" in the database$/
+          # Do nothing
+        else
+          raise e
+        end
+      end
+      
+      if defined?(Delayed::MessageSending) && !Rails.env.test?
+        handle_asynchronously :subscribe_to_mailchimp
+        handle_asynchronously :update_mailchimp_subscription
+        handle_asynchronously :unsubscribe_from_mailchimp
+      end
       
       module ClassMethods
         
